a3a6fc9e298748625feb0fb00cccf097fa73bd9e
skylinebnr334 <168154498+skylinebnr334@users.noreply.github.com>
2025-08-20 17:42:26 +0900

    fx2

---
+++ src/hooks/useRound2_MultiData.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { Round2_Data_Entry } from "../types/Round2_Data_Entry";
+import { Round2_Data_Naibu } from "../types/Round2_Data_Naibu";
+
+export default function useRound2_MultiData(){
+    const [Round2_MultiData,setRound2MultiData]=useState<Round2_Data_Naibu>(
+        {
+            teams:[
+            {team_id:0,
+                current_phase:0,
+                latest_down_num:-1,
+                miss_timing:-1
+            },
+            {team_id:1,
+                current_phase:0,
+                latest_down_num:-1,
+                miss_timing:-1
+            },
+            {team_id:2,
+                current_phase:0,
+                latest_down_num:-1,
+                miss_timing:-1
+            },
+            {team_id:3,
+                current_phase:0,
+                latest_down_num:-1,
+                miss_timing:-1
+            },
+            {team_id:4,
+                current_phase:0,
+                latest_down_num:-1,
+                miss_timing:-1
+            }
+        ]
+        }
+    );
+    const getRound2_MultiData=()=>{
+
+    }
+    return{Round2_MultiData,getRound2_MultiData};
+}
---
+++ src/types/Round2_Data_Naibu.tsx
@@ -0,0 +1,5 @@
+import { Round2_Data_Entry } from "./Round2_Data_Entry";
+
+export type Round2_Data_Naibu={
+    teams:Array<Round2_Data_Entry>
+};
